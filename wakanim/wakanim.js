cae92fc6a90e36bdbb627ca9f2f9ce1d724d2fe8
nqueudot <79583794+nqueudot@users.noreply.github.com>
2021-03-23 09:18:49 +0100

    Add files via upload

---
+++ wakanim/wakanim.js
@@ -0,0 +1,226 @@
+const anime = [
+  {
+    titre:
+      "2.43: Seiin High School Boys Volleyball Club",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/31_637452788683704823243_Small_hq.jpg",
+    synopsis:
+      "Haijima Kimichiko était un passeur de talent au sein d’un collège réputé de Tokyo. Mais de profonds différends avec son équipe l’obligent à rejoindre la ville de sa mère, au sein de Fukui, là où il avait passé son enfance. Il y retrouve alors un vieil ami, Kuroba Yuni. Kuroba, doté d’incroyables capacités physiques, mais peu résistant au stress, et Haijima, dont la passion dévorante est source de conflits, vont devenir le pivot de l’équipe de volley de leur collège. Mais lors de leur dernier tournoi, le duo se déchire et s’évite jusqu’à leur entrée au sein du lycée de Seiin. Mais de nouveaux visages sont prêts à les accueillir dans une nouvelle équipe !",
+  },
+  {
+    titre: "BACK ARROW",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/111_637459742730923007BackArrow_Small-2_hq.jpg",
+    synopsis:
+      "Ringerind est une terre entourée d'un gigantesque mur. Ce dernier l'enveloppe, la protège et contribue à son développement, au point d’en être déifié ; c’est l'essence même de cette terre. Un jour, un mystérieux homme nommé Back Arrow apparaît dans un village reculé du nom d’Edger. Arrow est amnésique, mais il certifie venir de l’autre côté du mur. Il décide alors d’aller au-delà de ce dernier afin de recouvrer la mémoire, mais se retrouve peu à peu mêlé à un conflit dont il semble être le centre…",
+  },
+  {
+    titre:
+      "Bottom-tier Character Tomozaki",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/31_637452759580651964BTC_Small_hq.jpg",
+    synopsis:
+      "Tomozaki Fumiya est à la fois l'un des meilleurs gamers du Japon et un lycéen des plus solitaires. Un jour, celui qui voit la vie comme un jeu sans intérêt rencontre l'héroïne parfaite de son établissement, Hinami Aoi... qui lui ordonne de prendre sa vie en main aussi sérieusement qu'il vit ses parties de jeu vidéo ! La vie est-elle un jeu sans intérêt ou le plus fin des plaisirs ? Avec Hinami aux manettes, une petite révolution s'amorce dans l'existence de Tomozaki !",
+  },
+  {
+    titre: "Gekidol",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/31_637452819615489102AP_Small_hq.jpg",
+    synopsis:
+      "Cinq ans se sont écoulés depuis un mystérieux désastre ayant rayé plusieurs villes de la carte. Si le monde est plongé dans le chaos, les sociétés essayent peu à peu de se reconstruire. C'est dans cet environnement que voit le jour le système holographique théâtral, où de jeunes filles se produisent sur des scènes spectaculaires afin de redonner le sourire à des spectateurs. Emplies d'espoir, elles n'attendent qu'une chose : que le rideau se lève !",
+  },
+  {
+    titre: "When They Cry, When the Cicadas Cry, The Moment the Cicadas Cry",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/910_637378275236244361Higurashi-2_Small_hq.jpg",
+    synopsis:
+      "Loin de la métropole, au milieu d’une nature abondante, se trouve un village reclus du nom de Hinamizawa. On raconte que ce dernier aurait dû disparaître par le passé, submergé au fond d’un barrage. Cependant, le village a subsisté, et se voit même accueillir un nouvel élève en cours d’année scolaire, nommé Maebara Keiichi. Keiichi étant habitué à la vie urbaine, il apprécie son nouveau quotidien paisible entouré de ses amis, espérant que ce moment durera éternellement… du moins, jusqu’au jour du festival annuel du village, Watanagashi. Au mois de juin de l’an 1983, pendant la saison du chant des cigales, la vie des habitants est soudainement bouleversée par une interminable succession d’événements tragiques…",
+  },
+  {
+    titre: "Horimiya",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/21_637452072151948445Horimiya_Small_hq.jpg",
+    synopsis:
+      "Hori Kyôko est la coqueluche de sa classe, une lycéenne jolie et populaire aux excellents résultats. Mais le soir venu, elle doit tenir la maison à la place de ses parents qui travaillent et s'occuper de son petit frère, Sôta. Un jour pas comme un autre, Sôta se fait mal dehors. Un garçon inconnu qui prend sur lui de le ramener chez sa famille, mais quand Kyôko leur ouvre la porte, c'est interloqué qu'il prononce son nom avec un soupçon d'hésitation. Elle se rend alors compte que la personne qui vient de ramener son frère n'est autre qu'un camarade de classe... Leur rencontre sera-t-elle une porte vers une nouvelle amitié ou des sentiments plus forts encore ? Découvrez une histoire adolescente aussi pétillante que douce-amère !",
+  },
+  {
+    titre: "Horsaga",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/51_637454596394661523Hortensia_Saga-2_Small_hq.jpg",
+    synopsis:
+      "Le royaume d'Hortensia sur la péninsule de Iepetus est fier de son histoire vieille de 700 ans. Les terres fertiles de ce royaume ont toujours été convoitées par les pays voisins, mais le royaume pouvait compter sur ses deux grands duchés Olivier et Camellia pour repousser les menaces d'invasion. Hélas, le cinquième jour du douzième mois de la 767e année du calendrier du Saint Souverain, le grand duché de Camellia s'est soudainement retourné contre le royaume d'Hortensia. Des monstres sont apparus au moment de la trahison du duché de Camellia, plongeant ainsi le royaume dans une ère de chaos. Les chevaliers doivent affronter leur destin dans ce monde transformé en champ de bataille. La bataille fatidique pour la succession du royaume, qui a débuté par une grande tragédie, reprend de plus belle.",
+  },
+  {
+    titre:
+      "Imagine un Cambrousard du Dernier Donjon dans la Ville de Départ !",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/21_637452048020183333The-Last-Dungeon_Small_hq.jpg",
+    synopsis:
+      "Le jeune Lloyd vit dans un village très très éloigné de la capitale. Il rêve de devenir soldat, mais est de son propre aveu le plus faible garçon du village. De ce fait, les autres villageois désapprouvent fermement ses ambitieux. Ça ne suffit pas à décourager Lloyd, qui part rejoindre la capitale. Toutefois, aussi bien Lloyd que le village ignoraient une vérité cruciale… Ce sont les descendants d'un héros qui a jadis sauvé le monde, et possèdent une puissance bien plus grande que les gens ordinaires. Bien qu'étant le plus faible de son village, la force de Lloyd est déjà totalement hors normes.",
+  },
+  {
+    titre: "Kemono Jihen",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/41_637453581125831396Kemono_Small_FR_hq.jpg",
+    synopsis:
+      "Depuis des temps immémoriaux, les êtres nommés Kemonos vivent parmi les humains, dissimulés dans l'ombre. Beaucoup d'entre eux se sont adaptés à l'évolution humaine et se sont mêlés à la société. Mais récemment, on rapporte de nombreux cas de Kemonos liés bien plus étroitement aux humains que nécessaire. Inugami, qui dirige une agence de détectives privés, s'occupe d'incidents liés à ces Kemonos. L'une de ses affaires le mène dans un petit village perdu, où il va faire la rencontre d'un jeune garçon nommé Kabane…",
+  },
+  {
+    titre: "King's Raid: Ishi o Tsugumono-tachi",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/2010_637388046208766434Kings-Raid_Small_hq.jpg",
+    synopsis:
+      "Le continent Orbis, qui occupe plus de la moitié du monde créé par la déesse de la lumière Lua, est le berceau des humains et de nombreuses autres races. Il y a 100 ans, Sa Majesté le roi Kyle, souverain du royaume d’Orvelia, a combattu l’invasion du roi des démons Angmund venu tout droit du monde démoniaque grâce à l’épée sacrée Aea. Avec l’aide de compagnons de différentes races, il repousse l’armée de démons et bannit Angmund, mais disparaît lors de la dernière bataille.",
+  },
+  {
+    titre: "Shingeki no Kyojin - Attack on Titan",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/186_636964446993572590SNK_S1_Small_FR_hq.jpg",
+    synopsis:
+      "Dans un monde ravagé par des titans mangeurs d’homme depuis plus d’un siècle, les rares survivants de l’Humanité n’ont d’autre choix pour survivre que de se barricader dans une cité-forteresse. Le jeune Eren, témoin de la mort de sa mère dévorée par un titan, n’a qu’un rêve : entrer dans le corps d’élite chargé de découvrir l’origine des titans, et les annihiler jusqu’au dernier…",
+  },
+  {
+    titre: "Soukou Musume Senki",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/31_637452785940065931SMS_Small_hq.jpg",
+    synopsis:
+      "Le quotidien de cinq jeunes filles est bouleversé lorsqu'elles se retrouvent équipées d'armures du nom de LBX Unit, devenant des « LBX Girls ». Elles ont chacune été arrachées à leurs propres dimensions pour accomplir une mission précise : anéantir les Mimesis, une forme de vie mécanique menaçant toutes les dimensions. Ces jeunes héroïnes ayant traversé l'espace-temps devront vivre comme des mercenaires et comprendre au péril de leur vie le sens des mots espoir et désespoir à l'échelle de l'humanité entière.",
+  },
+  {
+    titre: "Cells at Work - Hataraku Saibou",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/206_636966581862514545Cells_at_Work_Small_hq.jpg",
+    synopsis:
+      "Cette histoire est la vôtre : c'est celle de votre corps. Le corps humain est composé d’approximativement 37 billions de cellules. Chaque jour, à chaque instant, chacune d'elle travaille dur pour protéger leur monde qu'est le corps humain. Des globules rouges qui transportent l’oxygène aux globules blancs qui combattent les bactéries, découvrez ces héros anonymes et les aventures qui se déroulent à l'intérieur de vous ! Ceci est l’histoire étrange et familière de la vie des cellules.",
+  },
+  {
+    titre: "はたらく細胞BLACK",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/221_637469239592252614CAT-B_Small_FR-DUB_hq.jpg",
+    synopsis:
+      "Il était une fois... l'histoire de votre corps. Les hématies ont une mission : transporter de l'oxygène dans le corps, jour après jour. Malheureusement pour elle, notre héroïne du jour vit un enfer quotidien dans un corps où les conditions de travail sont épouvantables. Alcool, clopes, stress, manque de sommeil, rien ne lui est épargné, mais comment les cellules vivent-elles leur labeur dans un environnement aussi détérioré ?",
+  },
+  {
+    titre: "Log Horizon 3rd Season, Log Horizon Third Season",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/21_637452043177020276Log_Horizon_Small_hq.jpg",
+    synopsis:
+      "Cela fait à présent un an que Shiroe et ses amis sont piégés dans Akiba à cause de la Catastrophe. La création de la Table ronde a apporté l'ordre et la prospérité aux habitants, mais des intrigues politiques et la menace permanente que représentent de nouveaux monstres menacent de détruire tout ce pour quoi Shiroe s'est battu. Jamais la Table ronde n'a été aussi proche de sa fin...",
+  },
+  {
+    titre: "Mushoku Tensei, Mushoku Tensei: Isekai Ittara Honki Dasu",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/221_637469235946244348MT_Small_SUB-FR_hq.jpg",
+    synopsis:
+      "Ici, je vais me transcender ! Un anonyme de 34 ans, célibataire endurci, reclus et au chômage se fait écraser par un camion peu après avoir été chassé de la maison familiale. Quelle ne fut pas sa surprise lorsqu'il se réveilla bébé dans un monde d'épées et de magie ! Rudeus, comme il s'appelait désormais, jura alors de réussir sa deuxième vie et de tirer un trait sur l'immonde déchet qu'il avait été 34 ans durant. Dès son jeune âge, il nouera de nombreux liens dans ce nouveau monde, notamment une solide amitié avec trois fillettes, avant de partir pour une aventure aussi palpitante que périlleuse. Une seconde chance lui a été accordée... et elle ne sera pas de tout repos !",
+  },
+  {
+    titre: "Urasekai Picnic",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/21_637452087801527635OP_Small_hq.jpg",
+    synopsis:
+      "Le Sinueux, Dame Hasshaku la gare de Kisaragi… Ces créatures dangereuses de légendes urbaines apparaissent dans le versomonde. C'est un monde voisin du nôtre et empli de mystères. Kamikoshi Sorawo a découvert le versomonde derrière la porte d'un bâtiment abandonné, et y fait la connaissance de Nishina Toriko. Cette rencontre va totalement bouleverser la vie de Sorawo. Toriko demande à Sorawo de l'aider à explorer le versomonde. Elle y recherche une jeune fille disparue nommée Satsuki. Sorawo accepte d'aider Toriko pour pouvoir explorer et étudier cet autre monde, mais aussi gagner de l'argent. C'est le début pour elles d'une lutte pour leur survie dans un monde inconnu empli de créatures étranges.",
+  },
+  {
+    titre: "Project Scard: Scar on the Praeter, Project Scard: Praeter no Kizu",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/31_637452819945300203Scar-OTP_Small_hq.jpg",
+    synopsis:
+      "Bienvenue à Tokyo, dans le quartier d'Akatsuki. Abandonné par la nation, ce quartier sans loi aucune est protégé par trois organisations qui s'affrontent et tentent de faire régner l'ordre : Hélios, Artémis et le Bureau de la Sécurité Publique. Elles emploient toutes des Scard, des êtres marqués par un tatouage divin qui les rendraient insensibles aux balles et aux armes blanches…",
+  },
+  {
+    titre: "SK∞, SK8, SK Eight",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/21_637452075376349736SK8_Small_hq.jpg",
+    synopsis:
+      "« Ce jour-là, je l’ai vue de mes propres yeux. De la neige tombait à Okinawa, dans le sud du Japon… » Un jeune lycéen en première mordu de skate nommé Reki est passionné par la course « S » qui se déroule dans une vieille mine abandonnée. C’est une course dangereuse tenue dans le plus grand secret où tous les coups sont permis, et les duels qui s’y déroulent sont très appréciés du public.",
+  },
+  {
+    titre: "Skate-Leading Stars",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/2312_637443249216839953SLS_Small_hq.jpg",
+    synopsis:
+      "« Tu ne me battras jamais. » Cette phrase, prononcée par son rival Shinozaki Reo, a poussé le jeune Maeshima Kensei à mettre un terme à sa carrière de patineur. Plusieurs années plus tard, Maeshima, désormais lycéen, met à profit ses qualités physiques naturelles pour dépanner les différents clubs de son lycée, tout en refusant de se consacrer pleinement à un sport en particulier. Un jour, il apprend que son ancien rival Shinozaki abandonne le patinage artistique individuel pour se lancer dans une discipline collective, le skate-leading. Peu après, un jeune homme du nom de Sasugai Hayato aborde Maeshima : il affirme avoir besoin de l’ex-patineur et l’invite à le rejoindre dans le monde du skate-leading !",
+  },
+  {
+    titre: "Majutsushi Orphen Hagure Tabi",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/71_637140089924674557Orphen_Small_hq.jpg",
+    synopsis:
+      "Notre histoire prend place sur un ancien continent où prospèrent aujourd'hui les humains, en lieu et place des dragons des temps anciens. Le jeune Krylancelo Finlandi vit avec sa sœur Azalie à la Tour du Croc, un institut d'apprentissage de la sorcellerie, mais un jour, Azalie est maudite et disparaît. Krylancelo se lance alors dans un voyage afin de la retrouver. Cinq ans plus tard, désormais nommé Orphen, il exerce illégalement l'activité de prêteur. Un jour, avec le dénommé Volkan, il se rend chez les Everlasting, une arnaque autour du mariage de leur fille aînée en tête…",
+  },
+  {
+    titre: "Yakusoku no Neverland",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/41_636822051567388188PN_Small_hq.jpg",
+    synopsis:
+      "Celle qu’ils aiment comme leur mère n’est pas leur vraie maman. Les enfants qui vivent tous ensemble dans ce lieu ne sont pas vraiment frères et sœurs. Gracefield House est un orphelinat, une maison unique en son genre où s’épanouissent trente-huit frères et sœurs sans liens de sang, élevés par Maman. Cependant, leur charmant quotidien va brutalement prendre fin…",
+  },
+  {
+    titre: "Wandā Eggu Puraioriti, WEG",
+    image:
+      "https://d2y6mqrpjbqoe6.cloudfront.net/image/upload/f_auto,q_auto/media/library-400/31_637452813687661460WEP_Small_hq.jpg",
+    synopsis:
+      "Lors d'une promenade nocturne, Ohto Ai, une jeune fille de 14 ans, entend une mystérieuse voix qui lui confie l'Egg. Si tu veux changer le futur... Alors, fais un choix. Allez, crois en toi et casse l'Egg. Que va-t-il arriver à Ai une fois qu'elle aura cassé l'Egg ?",
+  },
+];
+
+let buttonMoin = document.getElementById("buttonMoin");
+let buttonPlus = document.getElementById("buttonPlus");
+
+const carousell = document.getElementById("carousell");
+let image = document.getElementsByClassName("image");
+let trueImg = document.querySelectorAll("img");
+let synopsis = document.getElementById("text");
+
+let e = 0;
+let grande = 2;
+
+console.log(25);
+
+for (let i = 0; i < anime.length; i++) {
+  const carImage = document.createElement("div");
+  carImage.classList.add("image");
+  carousell.appendChild(carImage);
+
+  const titre = document.createElement("h1");
+  titre.innerHTML = anime[i].titre;
+  carImage.appendChild(titre);
+
+  const carChange = document.createElement("img");
+  carChange.src = anime[i].image;
+  carImage.appendChild(carChange);
+}
+
+image[2].style.transform = "scale(1.4)";
+synopsis.innerHTML = anime[grande].synopsis;
+
+buttonPlus.addEventListener("click", function () {
+  if (grande > 0) {
+    e = e + 17;
+    grande--;
+    console.log(grande);
+
+    for (let i = 0; i < image.length; i++) {
+      image[i].style.transform = `translateX(${e}vw)`;
+      image[i].style.transition = "all 0.5s ease";
+    }
+    image[grande].style.transform = ` translateX(${e}vw) scale(1.4)`;
+    synopsis.innerHTML = anime[grande].synopsis;
+  }
+});
+
+buttonMoin.addEventListener("click", function () {
+  if (grande < anime.length - 1) {
+    e = e - 17;
+    grande++;
+    console.log(grande);
+
+    for (let i = 0; i < image.length; i++) {
+      image[i].style.transform = `translateX(${e}vw)`;
+      image[i].style.transition = "all 0.5s ease";
+    }
+    image[grande].style.transform = `translateX(${e}vw) scale(1.4)`;
+    synopsis.innerHTML = anime[grande].synopsis;
+  }
+});
