413c2ed573304ff22ed45f1599fbc6ffe7afa1fa
LegeayP <73394194+LegeayP@users.noreply.github.com>
2021-03-23 16:21:40 +0100

    Update ADN.js

--- ADN.js
+++ ADN.js
@@ -1,167 +1,188 @@
 const anime = [
-    {
-        titre:
-        "BORUTO - NARUTO NEXT GENERATIONS",
-        image:
-            "https://image.animedigitalnetwork.fr/license/boruto/tv/web/affiche_370x0.jpg",
-        synopsis:
-            "Les grands affrontements dans le monde des ninjas appartiennent désormais au passé. Le village de Konoha est entré dans une ère de paix. Boruto, le fils du 7e Hokage, vit mal d’être dans l’ombre de son éminent paternel. Il ne se doute pas qu’une nouvelle menace plane sur eux… La légende de la nouvelle génération commence maintenant ! !"
+  {
+      titre:
+      "BORUTO - NARUTO NEXT GENERATIONS",
+      image:
+          "https://image.animedigitalnetwork.fr/license/boruto/tv/web/affiche_370x0.jpg",
+      synopsis:
+          "Les grands affrontements dans le monde des ninjas appartiennent désormais au passé. Le village de Konoha est entré dans une ère de paix. Boruto, le fils du 7e Hokage, vit mal d’être dans l’ombre de son éminent paternel. Il ne se doute pas qu’une nouvelle menace plane sur eux… La légende de la nouvelle génération commence maintenant ! !",
+      link:
+          "https://animedigitalnetwork.fr/video/boruto-naruto-next-generations",
+
     }, 
 
-    {
-        titre:
-        "DR. STONE - SAISON 2 - STONE WARS",
-      image:
-        "https://i0.wp.com/crfeaturesbiz.wpcomstaging.com/wp-content/uploads/2020/12/20_12_Dr.STONE_S2_SeasonalAssets_2x3.jpg?w=1003&ssl=1",
-      synopsis:
-        "Plusieurs milliers d'années après un mystérieux phénomène qui a transformé toute l'humanité en pierre, Senku, un lycéen extrêmement intelligent et animé par un esprit scientifique, se réveille. Face à ce monde figé, où toutes les civilisations se sont effondrées, il décide d'utiliser la science pour le reconstruire. Avec l'aide de son ami d'enfance, l'infatigable Taiju Ôki qui s'est lui aussi réveillé, ils vont devoir repartir de zéro. Ainsi commence une incroyable aventure pour se réapproprier des milliers d'années d'histoire de la science, de l'Âge de pierre à leurs jours…",
-    },
-    {
-        titre:
-        "DÉTECTIVE CONAN",
-      image:
-        "https://image.animedigitalnetwork.fr/license/conan/tv/web/affiche_370x0.jpg",
-      synopsis:
-        "L’histoire nous entraîne dans le quotidien du jeune détective Shinnichi Kudo, qui, après avoir croisé la route des mystérieux « hommes en noirs » a été contraint d’avaler de force du poison et s’est retrouvé rajeuni de 10 ans. C’est sous cette apparence d’enfant de 7 ans que le détective Conan Edogawa entre en scène et se lance à la poursuite de l’organisation secrète…",
-    },
-    {
-        titre:
-        "RE:ZERO –STARTING LIFE IN ANOTHER WORLD– SAISON 2",
-      image:
-        "https://i1.wp.com/crfeaturesbiz.wpcomstaging.com/wp-content/uploads/2020/12/20_12_CR_ReZero_S2C2_SeasonalAssets_2x3-1-1.png?resize=1024%2C1536&ssl=1",
-      synopsis:
-        "Subaru Natsuki a basculé dans un monde fantastique où il fait la connaissance d’Émilia, une jeune fille aux longs cheveux d’argent qu’il jure de protéger. Malheureusement, le jeune homme ne résiste pas longtemps en se faisant tuer rapidement. Pourtant, il revient d’entre les morts, un phénomène qui se répète sans cesse, le ramenant toujours à son point de départ. Subaru entame alors un combat perpétuel dans lequel il essaie, peu à peu, de changer le futur...",
-    },
-    {
-        titre:
-        "DIGIMON ADVENTURE (2020)",
-      image:
-        "https://image.animedigitalnetwork.fr/license/digimon/tv9/web/affiche_370x0.jpg",
-      synopsis: "Alors que Taichi Yagami se prépare pour aller à son camp de vacances, une puissante cyberattaque frappe Tokyo et paralyse la ville. Sa mère et sa sœur cadette Hikari, parties pour Shibuya, sont bloquées sur la ligne circulaire de train qui est hors de contrôle : Taichi part les sauver mais il est soudainement transporté dans le Digimonde.",
-    },
-    {
-        titre:
-        "DRAGON QUEST THE ADVENTURE OF DAI",
-      image:
-        "https://image.animedigitalnetwork.fr/license/dragonquest/tv2/web/affiche_370x0.jpg",
-      synopsis: "Daï vit sur l'île de Dermline, et malgré son admiration pour les héros, il suit un entraînement pour devenir mage auprès de son père adoptif, le magimol Brass. Un jour, l'arrivée d'une bande de compagnons menée par un héros à bord d'un bateau remplit Daï d'excitation, mais il s'agissait malheureusement d'un faussaire. Son objectif ? S'emparer d'une créature mythique unique en son genre, le gluant de métal doré, Goméchan. Afin de récupérer son meilleur ami, Dai part à la poursuite du faux héros et de sa bande, armé des tubes magiques que lui a donnés Brass. Pendant ce temps, sur le navire, Dérolin se vante de la capture de Goméchan au roi de Romos, mais Daï le rattrape et parvient à récupérer Goméchan avec l'aide des monstres enfermés dans les tubes magiques. Ému par le courage de Daï au combat, le roi de Romos félicite ce dernier et lui confère la Tiare du conquérant, mais il doit bien vite revenir à son entraînement de magie. C'est alors qu'un autre navire arrive sur l'île, avec à son bord l'évêque Temjin, Baron le sage et la princesse Léona.",
-    },
-    {
-        titre:
-        "HEAVEN'S DESIGN TEAM",
-      image:
-        "https://image.animedigitalnetwork.fr/license/heavendesign/tv/web/affiche_370x0.jpg",
-      synopsis: "Suivez les meilleurs designers du département de design animalier de Heaven alors qu'ils tentent de conclure un accord avec leur plus gros client : DIEU.",
-    },
-    {
-        titre:
-        "ONE PIECE : ARC 16 - PAYS DE WANO",
-      image:
-        "https://image.animedigitalnetwork.fr/license/onepiece/tv/web/affiche_370x0.jpg",
-      synopsis: "Oyez ! Oyez ! Pirates de tous bords ! Gol D.Roger, le légendaire Roi des pirates, mort sur l’échafaud, a laissé derrière lui un trésor inestimable : le « One Piece ». Monkey D.Luffy, un garçon naïf et impulsif, rêve de mettre la main sur ce fameux butin et de devenir le nouveau Roi des Pirates. Parcourant les mers, il rassemble au fil de l’eau un équipage redoutable. Luffy et ses compagnons débarquent au pays de Wano pour affronter le terrible Kaido, réputé pour être la personne la plus forte du monde. Là-bas, ils y retrouvent Zoro et le reste de l'équipage infiltrés dans tout le pays...",
-    },
-    {
-        titre:
-        "WORLD TRIGGER - SAISON 2",
-        image:
-          "https://image.animedigitalnetwork.fr/license/worldtrigger/tv2/web/affiche_370x0.jpg",
-        synopsis: "Le jeune Osamu Mikumo fait équipe avec l'un d'entre eux, Yuma Kuga, et une amie d'enfance, Chika Amatori. Ils s'efforcent de gra- vir les échelons du Border pour rejoindre les équipes d'exploration de l'autre monde, Neighborhood. Pendant ce temps, une autre attaque a été détectée.",
-      },
-      {
-          titre:
-          "YASHAHIME: PRINCESS HALF-DEMON",
-      image:
-      "https://image.animedigitalnetwork.fr/license/yashahime/tv/web/affiche_370x0.jpg",
-    synopsis: "Dans le Japon féodal, les jumelles hanyô (mi-yôkai, mi-humaines) Towa et Setsuna sont séparées lors de l'attaque d'un yôkai. Alors qu'elle cherche désespérément sa jeune soeur, Towa se retrouve dans un mystérieux tunnel qui l'envoie dans le Japon d'aujourd'hui où elle est élevée par le frère de Kagome Higurashi, Sôta et sa famille.",
+  {
+      titre:
+      "DR. STONE - SAISON 2 - STONE WARS",
+    image:
+      "https://i0.wp.com/crfeaturesbiz.wpcomstaging.com/wp-content/uploads/2020/12/20_12_Dr.STONE_S2_SeasonalAssets_2x3.jpg?w=1003&ssl=1",
+    synopsis:
+      "Plusieurs milliers d'années après un mystérieux phénomène qui a transformé toute l'humanité en pierre, Senku, un lycéen extrêmement intelligent et animé par un esprit scientifique, se réveille. Face à ce monde figé, où toutes les civilisations se sont effondrées, il décide d'utiliser la science pour le reconstruire. Avec l'aide de son ami d'enfance, l'infatigable Taiju Ôki qui s'est lui aussi réveillé, ils vont devoir repartir de zéro. Ainsi commence une incroyable aventure pour se réapproprier des milliers d'années d'histoire de la science, de l'Âge de pierre à leurs jours…",
+      link:
+      "https://animedigitalnetwork.fr/video/drstone_tv2",
   },
+  {
+      titre:
+      "DÉTECTIVE CONAN",
+    image:
+      "https://image.animedigitalnetwork.fr/license/conan/tv/web/affiche_370x0.jpg",
+    synopsis:
+      "L’histoire nous entraîne dans le quotidien du jeune détective Shinnichi Kudo, qui, après avoir croisé la route des mystérieux « hommes en noirs » a été contraint d’avaler de force du poison et s’est retrouvé rajeuni de 10 ans. C’est sous cette apparence d’enfant de 7 ans que le détective Conan Edogawa entre en scène et se lance à la poursuite de l’organisation secrète…",
+    link:
+      "https://animedigitalnetwork.fr/video/detective-conan",
+  },
+  {
+      titre:
+      "RE:ZERO –STARTING LIFE IN ANOTHER WORLD– SAISON 2",
+    image:
+      "https://i1.wp.com/crfeaturesbiz.wpcomstaging.com/wp-content/uploads/2020/12/20_12_CR_ReZero_S2C2_SeasonalAssets_2x3-1-1.png?resize=1024%2C1536&ssl=1",
+    synopsis:
+      "Subaru Natsuki a basculé dans un monde fantastique où il fait la connaissance d’Émilia, une jeune fille aux longs cheveux d’argent qu’il jure de protéger. Malheureusement, le jeune homme ne résiste pas longtemps en se faisant tuer rapidement. Pourtant, il revient d’entre les morts, un phénomène qui se répète sans cesse, le ramenant toujours à son point de départ. Subaru entame alors un combat perpétuel dans lequel il essaie, peu à peu, de changer le futur...",
+      link:
+      "https://animedigitalnetwork.fr/video/rezero_tv2",
+  },
+  {
+      titre:
+      "DIGIMON ADVENTURE (2020)",
+    image:
+      "https://image.animedigitalnetwork.fr/license/digimon/tv9/web/affiche_370x0.jpg",
+    synopsis: "Alors que Taichi Yagami se prépare pour aller à son camp de vacances, une puissante cyberattaque frappe Tokyo et paralyse la ville. Sa mère et sa sœur cadette Hikari, parties pour Shibuya, sont bloquées sur la ligne circulaire de train qui est hors de contrôle : Taichi part les sauver mais il est soudainement transporté dans le Digimonde.",
+    link:
+    "https://animedigitalnetwork.fr/video/digimon-adventure",
+  },
+  {
+      titre:
+      "DRAGON QUEST THE ADVENTURE OF DAI",
+    image:
+      "https://image.animedigitalnetwork.fr/license/dragonquest/tv2/web/affiche_370x0.jpg",
+    synopsis: "Daï vit sur l'île de Dermline, et malgré son admiration pour les héros, il suit un entraînement pour devenir mage auprès de son père adoptif, le magimol Brass. Un jour, l'arrivée d'une bande de compagnons menée par un héros à bord d'un bateau remplit Daï d'excitation, mais il s'agissait malheureusement d'un faussaire. Son objectif ? S'emparer d'une créature mythique unique en son genre, le gluant de métal doré, Goméchan. Afin de récupérer son meilleur ami, Dai part à la poursuite du faux héros et de sa bande, armé des tubes magiques que lui a donnés Brass. Pendant ce temps, sur le navire, Dérolin se vante de la capture de Goméchan au roi de Romos, mais Daï le rattrape et parvient à récupérer Goméchan avec l'aide des monstres enfermés dans les tubes magiques. Ému par le courage de Daï au combat, le roi de Romos félicite ce dernier et lui confère la Tiare du conquérant, mais il doit bien vite revenir à son entraînement de magie. C'est alors qu'un autre navire arrive sur l'île, avec à son bord l'évêque Temjin, Baron le sage et la princesse Léona.",
+    link:
+    "https://animedigitalnetwork.fr/video/Dragon-quest-adventure-of-dai-fly-daibouken",
+  },
+  {
+      titre:
+      "HEAVEN'S DESIGN TEAM",
+    image:
+      "https://image.animedigitalnetwork.fr/license/heavendesign/tv/web/affiche_370x0.jpg",
+    synopsis: "Suivez les meilleurs designers du département de design animalier de Heaven alors qu'ils tentent de conclure un accord avec leur plus gros client : DIEU.",
+    link:
+    "https://animedigitalnetwork.fr/video/heavens-design-team",
+  },
+  {
+      titre:
+      "ONE PIECE : ARC 16 - PAYS DE WANO",
+    image:
+      "https://image.animedigitalnetwork.fr/license/onepiece/tv/web/affiche_370x0.jpg",
+    synopsis: "Oyez ! Oyez ! Pirates de tous bords ! Gol D.Roger, le légendaire Roi des pirates, mort sur l’échafaud, a laissé derrière lui un trésor inestimable : le « One Piece ». Monkey D.Luffy, un garçon naïf et impulsif, rêve de mettre la main sur ce fameux butin et de devenir le nouveau Roi des Pirates. Parcourant les mers, il rassemble au fil de l’eau un équipage redoutable. Luffy et ses compagnons débarquent au pays de Wano pour affronter le terrible Kaido, réputé pour être la personne la plus forte du monde. Là-bas, ils y retrouvent Zoro et le reste de l'équipage infiltrés dans tout le pays...",
+    link:
+    "https://animedigitalnetwork.fr/video/one-piece",
+  },
+  {
+      titre:
+      "WORLD TRIGGER - SAISON 2",
+      image:
+        "https://image.animedigitalnetwork.fr/license/worldtrigger/tv2/web/affiche_370x0.jpg",
+      synopsis: "Le jeune Osamu Mikumo fait équipe avec l'un d'entre eux, Yuma Kuga, et une amie d'enfance, Chika Amatori. Ils s'efforcent de gra- vir les échelons du Border pour rejoindre les équipes d'exploration de l'autre monde, Neighborhood. Pendant ce temps, une autre attaque a été détectée.",
+      link:
+      "https://animedigitalnetwork.fr/video/world-trigger-saison-2",
+    },
+    {
+        titre:
+        "YASHAHIME: PRINCESS HALF-DEMON",
+    image:
+    "https://image.animedigitalnetwork.fr/license/yashahime/tv/web/affiche_370x0.jpg",
+  synopsis: "Dans le Japon féodal, les jumelles hanyô (mi-yôkai, mi-humaines) Towa et Setsuna sont séparées lors de l'attaque d'un yôkai. Alors qu'elle cherche désespérément sa jeune soeur, Towa se retrouve dans un mystérieux tunnel qui l'envoie dans le Japon d'aujourd'hui où elle est élevée par le frère de Kagome Higurashi, Sôta et sa famille.",
+  link:
+  "https://animedigitalnetwork.fr/video/yashahime_tv",
+},
 
-  ];
+];
+
+let buttonMoin = document.getElementsByClassName("buttonMoin");
+let buttonPlus = document.getElementsByClassName("buttonPlus");
+
+const carousell = document.getElementById("carousell");
+let image = document.getElementsByClassName("image");
+let trueImg = document.querySelectorAll("img");
+let synopsis = document.getElementById("text");
+
+let e = 0;
+let grande = 2;
+
+for (let i = 0; i < anime.length; i++) {
+  const carImage = document.createElement("div");
+  carImage.classList.add("image");
+  carousell.appendChild(carImage);
+
+  const titre = document.createElement("h1");
+  titre.innerHTML = anime[i].titre;
+  carImage.appendChild(titre);
+
+  const butt = document.createElement("section");
+  butt.classList.add("button");
+  carImage.appendChild(butt);
   
-  let buttonMoin = document.getElementsByClassName("buttonMoin");
-  let buttonPlus = document.getElementsByClassName("buttonPlus");
-  
-  const carousell = document.getElementById("carousell");
-  let image = document.getElementsByClassName("image");
-  let trueImg = document.querySelectorAll("img");
-  let synopsis = document.getElementById("text");
-  
-  let e = 0;
-  let grande = 2;
-  
-  for (let i = 0; i < anime.length; i++) {
-    const carImage = document.createElement("div");
-    carImage.classList.add("image");
-    carousell.appendChild(carImage);
-  
-    const titre = document.createElement("h1");
-    titre.innerHTML = anime[i].titre;
-    carImage.appendChild(titre);
-  
-    const butt = document.createElement("section");
-    butt.classList.add("button");
-    carImage.appendChild(butt);
-    
-    const buttOne = document.createElement("button");
-    buttOne.classList.add("buttonPlus");
-    buttOne.innerHTML = "⇠";
-    butt.appendChild(buttOne);
-  
-    const buttTwo = document.createElement("button");
-    buttTwo.classList.add("buttonMoin");
-    buttTwo.innerHTML = "⇢";
-    butt.appendChild(buttTwo);
-  
-    const link = document.createElement("a");
-    link.href = anime[i].link;
-    link.target = "blank"
-    carImage.appendChild(link);
-  
-    const carChange = document.createElement("img");
-    carChange.src = anime[i].image;
-    link.appendChild(carChange);
-  }
-  
-  let button = document.getElementsByClassName("button");
-  
-  image[grande].style.transform = "scale(1.4)";
-  button[grande].style.display = "flex";
-  synopsis.innerHTML = anime[grande].synopsis;
-  
-  for (let i = 0; i < button.length; i++) {
-    buttonPlus[i].addEventListener("click", function () {
-      if (grande > 0) {
-        e = e + 17;
-        grande--;
-  
-        for (let i = 0; i < image.length; i++) {
-          image[i].style.transform = `translateX(${e}vw)`;
-          image[i].style.transition = "all 0.5s ease";
-        }
-  
-        image[grande].style.transform = ` translateX(${e}vw) scale(1.4)`;
-        button[grande + 1].style.display = "none";
-        button[grande].style.display = "flex";
-        synopsis.innerHTML = anime[grande].synopsis;
+  const buttOne = document.createElement("button");
+  buttOne.classList.add("buttonPlus");
+  buttOne.innerHTML = "⇠";
+  butt.appendChild(buttOne);
+
+  const buttTwo = document.createElement("button");
+  buttTwo.classList.add("buttonMoin");
+  buttTwo.innerHTML = "⇢";
+  butt.appendChild(buttTwo);
+
+  const link = document.createElement("a");
+  link.href = anime[i].link;
+  link.target = "blank"
+  carImage.appendChild(link);
+
+  const carChange = document.createElement("img");
+  carChange.src = anime[i].image;
+  link.appendChild(carChange);
+}
+
+let button = document.getElementsByClassName("button");
+
+image[grande].style.transform = "scale(1.4)";
+button[grande].style.display = "flex";
+synopsis.innerHTML = anime[grande].synopsis;
+
+for (let i = 0; i < button.length; i++) {
+  buttonPlus[i].addEventListener("click", function () {
+    if (grande > 0) {
+      e = e + 17;
+      grande--;
+
+      for (let i = 0; i < image.length; i++) {
+        image[i].style.transform = `translateX(${e}vw)`;
+        image[i].style.transition = "all 0.5s ease";
       }
-    });
-  }
-  
-  for (let i = 0; i < button.length; i++) {
-    buttonMoin[i].addEventListener("click", function () {
-      if (grande < anime.length - 1) {
-        e = e - 17;
-        grande++;
-  
-        for (let i = 0; i < image.length; i++) {
-          image[i].style.transform = `translateX(${e}vw)`;
-          image[i].style.transition = "all 0.5s ease";
-        }
-  
-        image[grande].style.transform = `translateX(${e}vw) scale(1.4)`;
-        button[grande - 1].style.display = "none";
-        button[grande].style.display = "flex";
-        synopsis.innerHTML = anime[grande].synopsis;
+
+      image[grande].style.transform = ` translateX(${e}vw) scale(1.4)`;
+      button[grande + 1].style.display = "none";
+      button[grande].style.display = "flex";
+      synopsis.innerHTML = anime[grande].synopsis;
+    }
+  });
+}
+
+for (let i = 0; i < button.length; i++) {
+  buttonMoin[i].addEventListener("click", function () {
+    if (grande < anime.length - 1) {
+      e = e - 17;
+      grande++;
+
+      for (let i = 0; i < image.length; i++) {
+        image[i].style.transform = `translateX(${e}vw)`;
+        image[i].style.transition = "all 0.5s ease";
       }
-    });
-  }
+
+      image[grande].style.transform = `translateX(${e}vw) scale(1.4)`;
+      button[grande - 1].style.display = "none";
+      button[grande].style.display = "flex";
+      synopsis.innerHTML = anime[grande].synopsis;
+    }
+  });
+}
   
