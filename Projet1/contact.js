36fa75d90b7068a96a699365167c59f3715a90de
LegeayP <73394194+LegeayP@users.noreply.github.com>
2021-03-25 09:28:09 +0100

    Update contact.js

--- Projet1/contact.js
+++ Projet1/contact.js
@@ -29,3 +29,24 @@
     document.getElementById('idEmail').style.backgroundColor="#9C6";
     }
     }
+
+let burger = document.getElementsByClassName("burger");
+let nav = document.querySelector("nav");
+let flou = document.querySelector(".flou");
+
+console.log(burger);
+console.log(nav);
+console.log(flou);
+
+if (window.matchMedia("(max-width: 840px)").matches) {
+  burger[0].addEventListener("click", function () {
+    nav.style.transform = "translateX(0px)";
+    flou.style.display = "block";
+    flou.style.opacity = "0.3";
+  });
+  flou.addEventListener("click", function () {
+    nav.style.transform = "translateX(-30vw)";
+    flou.style.display = "none";
+    flou.style.opacity = "0";
+  });
+}
